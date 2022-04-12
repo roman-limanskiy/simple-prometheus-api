38ceba729d00b096022ebc5864e2ca0dae1605c5
Roman Limanskiy <rulimanskiy@gmail.com>
2022-04-12 09:22:19 +0500

    add new file

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+from flask import Flask
+from flask import request
+import json
+import subprocess
+import yaml
+
+
+app = Flask(__name__)
+workdir = '/opt/prometheus/alerting/'
+
+@app.route('/v1/alerting/', methods=['POST'])
+
+def main():
+        request_data = request.get_json(force=True)
+        result = {}
+
+        for key in request_data.keys():
+
+                if request_data[key]['action'] == 'create':
+                        alert_rule = { 'groups': [ { 'name': 'alert.rules', 'rules': [ { 'alert': 'InstanceDown', 'expr': 'probe_success{wifi_device_id = "' + request_data[key]['wifi_device_id'] + '"} == 0', 'for': request_data[key]['pending_time'] + 's' } ] } ] }
+                        with open( workdir + key + '.yaml', 'w') as outfile:
+                                yaml.dump( alert_rule, outfile )
+                        result[key] = 'created'
+
+                elif request_data[key]['action'] == 'update':
+                        alert_rule = { 'groups': { 'name': 'alert.rules', 'rules': { 'alert': 'InstanceDown', 'expr': 'probe_success{wifi_device_id = "' + request_data[key]['wifi_device_id'] + '"} == 0', 'for': request_data[key]['pending_time'] + 's' } } }
+                        with open( workdir + key + '.yaml', 'w') as outfile:
+                                yaml.dump( alert_rule, outfile )
+                        result[key] = 'updated'
+
+                elif request_data[key]['action'] == 'delete':
+                        subprocess.run( [ "rm", "-rf", workdir + key + ".yaml" ] )
+                        result[key] = 'deleted'
+
+                else:
+                        result[key] = 'unknown method'
+
+        return result
+
+if __name__ == "__main__":
+        app.run(host='10.101.15.224', port=9095)
